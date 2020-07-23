2e71a579819ddc100c537641799003e34fad6fba
Akuma <laoraulalejandrocipre@gmail.com>
2020-07-23 11:05:04 +0200

    incial commit

---
+++ login/login.Android/custom entry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using login.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry),typeof(custom_entry))]
+namespace login.Droid
+{
+    [Obsolete]
+    public class custom_entry : EntryRenderer
+    {
+        public custom_entry(Context context) : base(context)
+        {
+            
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || e.NewElement == null)
+            { return;
+            }
+            Control.Background = null;
+            if(!e.NewElement.IsPassword)
+            {
+                Control.InputType = InputTypes.TextFlagNoSuggestions;
+            }
+
+        }
+    }
+}
---
+++ login/login/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace login
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            Empresa.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+            Empresa.Stroke = Color.FromHex("#c7cdd2");
+        }
+
+        private void Entry_Focused_1(object sender, FocusEventArgs e)
+        {
+            Usuario.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused_1(object sender, FocusEventArgs e)
+        {
+            Usuario.Stroke = Color.FromHex("#c7cdd2");
+        }
+
+        private void Entry_Focused_2(object sender, FocusEventArgs e)
+        {
+            Password.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused_2(object sender, FocusEventArgs e)
+        {
+            Password.Stroke = Color.FromHex("#c7cdd2");
+        }
+    }
+}
